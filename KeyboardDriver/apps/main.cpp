6b49ad876cf2daef3714818dc3330e6b5d823204
Bartlomiej Sefanski <bartex00001@gmail.com>
2023-06-07 17:01:25 +0200

    Created a mock program to read keys pressed on a matrix

--- KeyboardDriver/apps/main.cpp
+++ KeyboardDriver/apps/main.cpp
@@ -1,26 +1,66 @@
-#include <stdio.h>
+#include <vector>
+#include <cstdint>
+#include <cstdio>
 
 #include <pico/stdlib.h>
 
-#include <KeyboardDriver/SignalEncoder.hpp>
+#include <KeyboardDriver/KeyMatrix.hpp>
+
+std::vector< std::int16_t > pressedKeys;
+
+void readMatrix();
 
 int main()
 {
     stdio_init_all();
 
-    gpio_init(25);
-    gpio_set_dir(25, GPIO_OUT);
+    initKeyMatrix();
+
+    // remember last poll time
+    std::uint64_t lastPollTime{ time_us_64() };
 
     while(true)
     {
-        printf("Led on\n");
-        gpio_put(25, 1);
-        sleep_ms(1000);
+        readMatrix();
 
-        printf("Led off\n");
-        gpio_put(25, 0);
-        sleep_ms(1000);
+        // std::uint64_t currentTime{ time_us_64() };
+        // std::uint64_t deltaTimeUs{ currentTime - lastPollTime };
+        // lastPollTime = currentTime;
+        // std::printf("\nLast poll time: %llu us", deltaTimeUs);
+
+        std::printf("\nPressed keys: ");
+        for(const auto key : pressedKeys)
+        {
+            if(key < 0)
+            {
+                std::printf("(mod)");
+            }
+
+            std::printf("%d     ", key);
+        }
+
+        sleep_ms(10);
+        pressedKeys.clear();
     }
 
     return 0;
+}
+
+void readMatrix()
+{
+    for(std::size_t i{}; i < outputLinesCount; ++i)
+    {
+        gpio_put(outputLines[i], 1);
+        sleep_us(1);
+
+        for(std::size_t j{}; j < inputLinesCount; ++j)
+        {
+            if(gpio_get(inputLines[j]))
+            {
+                pressedKeys.push_back(matrixCodes[j][i]);
+            }
+        }
+
+        gpio_put(outputLines[i], 0);
+    }
 }
--- KeyboardDriver/include/KeyboardDriver/KeyMatrix.hpp
+++ KeyboardDriver/include/KeyboardDriver/KeyMatrix.hpp
@@ -2,20 +2,19 @@
 
 #include <cstdint>
 
-constexpr std::size_t inputLinesCount = 3;
+constexpr std::size_t inputLinesCount = 2;
 constexpr std::uint8_t const inputLines[]{
-    18, 19, 20
+    13, 12
 };
 
-constexpr std::size_t outputLinesCount = 3;
+constexpr std::size_t outputLinesCount = 2;
 constexpr std::uint8_t const outputLines[]{
-    13, 12, 11
+    18, 19
 };
 
 constexpr std::int16_t matrixCodes[inputLinesCount][outputLinesCount]{
-    { 1, 2, 3 },
-    { 4, 5, 6 },
-    { -7, -8, -9 }
+    { 1, 2 },
+    { 3, -4 }
 };
 
 void initKeyMatrix();
